fcd2815ca098069114dd4b8e2d621a2c298a75a5
Ronan Giron <ElGigi@users.noreply.github.com>
2018-04-10 17:13:27 +0200

    Add ConfigAwareTrait and ConfigAwareInterface

---
+++ src/ConfigAwareTrait.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of Berlioz framework.
+ *
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @copyright 2017 Ronan GIRON
+ * @author    Ronan GIRON <https://github.com/ElGigi>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, to the root.
+ */
+
+namespace Berlioz\Config;
+
+/**
+ * Describes a config-aware instance.
+ */
+trait ConfigAwareTrait
+{
+    /** @var \Berlioz\Config\ConfigInterface Config */
+    private $config;
+
+    /**
+     * Get config.
+     *
+     * @return \Berlioz\Config\ConfigInterface|null
+     */
+    public function getConfig(): ?ConfigInterface
+    {
+        return $this->config;
+    }
+
+    /**
+     * Set config.
+     *
+     * @param \Berlioz\Config\ConfigInterface $config
+     *
+     * @return static
+     */
+    public function setConfig(ConfigInterface $config)
+    {
+        $this->config = $config;
+
+        return $this;
+    }
+
+    /**
+     * Has config?
+     *
+     * @return bool
+     */
+    public function hasConfig(): bool
+    {
+        return !is_null($this->config);
+    }
+}
